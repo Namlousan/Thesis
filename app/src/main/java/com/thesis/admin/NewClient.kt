582d482a78fcf8ff47b4af64ff4c7f3c4b8c34ab
pc <ignaciomarklouieo6411@gmail.com>
2021-05-15 17:04:49 +0800

    latest

--- app/src/main/java/com/thesis/admin/NewClient.kt
+++ app/src/main/java/com/thesis/admin/NewClient.kt
@@ -15,10 +15,8 @@ import org.apache.commons.lang3.RandomStringUtils
 
 class NewClient : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
-    var databaseReference : DatabaseReference? = null
-    var database: FirebaseDatabase? = null
-    private lateinit var tv : TextView
-    private lateinit var btn: Button
+    private var databaseReference : DatabaseReference? = null
+    private var database: FirebaseDatabase? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_client)
@@ -30,13 +28,13 @@ class NewClient : AppCompatActivity() {
         }
 
 
-        val button2 = findViewById<Button>(R.id.saveclient)
-        button2.setOnClickListener {
-            val code = findViewById<TextView>(R.id.clientcode)
-            val intent = Intent(this, Save_client::class.java)
-            intent.putExtra("result", code.getText());
-           startActivity(intent)
-        }
+//        val button2 = findViewById<Button>(R.id.saveclient)
+//        button2.setOnClickListener {
+//            val code = findViewById<TextView>(R.id.clientcode)
+//            val intent = Intent(this, Save_client::class.java)
+//            intent.putExtra("result", code.getText());
+//           startActivity(intent)
+//        }
 
         val button3 = findViewById<Button>(R.id.updateclient)
         button3.setOnClickListener {
@@ -69,7 +67,7 @@ class NewClient : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.email)
         val pass = findViewById<EditText>(R.id.pass)
         val textView =findViewById<TextView>(R.id.clientcode)
-
+     //   val code = findViewById<TextView>(R.id.clientcode)
         regButton.setOnClickListener{
 
             if (TextUtils.isEmpty(branch.text.toString())){
@@ -110,11 +108,13 @@ class NewClient : AppCompatActivity() {
                         currentUserDb?.child("lastname")?.setValue(lname.text.toString())
                         currentUserDb?.child("firtname")?.setValue(fname.text.toString())
                         currentUserDb?.child("email")?.setValue(email.text.toString())
-                        currentUserDb?.child("clientcode")?.setValue(pass.text.toString())
+                        currentUserDb?.child("clientcode")?.setValue(textView.text.toString())
+                        currentUserDb?.child("password")?.setValue(pass.text.toString())
                         Toast.makeText(this@NewClient, "Registration success!", Toast.LENGTH_LONG).show()
                         val intent = Intent(this, Save_client::class.java)
+                        intent.putExtra("result", textView.getText())
                         startActivity(intent)
-                        finish()
+                       // finish()
                     } else {
                         Toast.makeText(
                             this@NewClient,
